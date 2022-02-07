e32e278460a1fd678ffee3648f57d7f59c25c6ea
adrianaluisadc <adrianaluisadelacruz@gmail.com>
2022-02-07 17:34:01 +0800

    [feat] adding function for player's car movement

---
+++ CarLaneGame/src/constants.js
@@ -0,0 +1,11 @@
+//CONSTANTS
+
+const CAR_POSITION = 111; //USE FOR ADDING || SUBTRACTING CURRENT CAR POSITION
+
+const KEY_DOWN = { //THIS OBJECT'S USAGE IS TO SELECT THE KEYS
+    toLeft: "a", //property to move to the left
+    toRight: "d" //property to move to the right
+}
+
+//EXPORT THE CONSTANTS
+export { CAR_POSITION, KEY_DOWN };
--- CarLaneGame/src/game.js
+++ CarLaneGame/src/game.js
@@ -1,1 +1,77 @@
-const 
+//IMPORTATIONS
+
+import { CAR_POSITION, KEY_DOWN } from "./constants.js";
+
+// CREATING ELEMENTS
+
+const carPlayer = document.createElement('div');
+
+// ADDING CLASSLIST
+
+carPlayer.classList.add("car-player");
+
+
+//APPENDING CREATED ELEMENTS
+
+document.body.querySelector(".game").append(carPlayer);
+
+
+
+// FUNCTION FOR HIGHWAY MOVEMENT FROM TOP TO BOTTOM
+
+let highwayPosition = 0; 
+let gameSituation = true;
+
+function highwayMovement() {
+    if (gameSituation == true) {
+        highwayPosition = highwayPosition + 25;
+        document.body.querySelector(".game").style.backgroundPositionY = highwayPosition + "px";
+    }
+}
+
+
+
+//FUNCTION FOR MOVING PLAYER'S CAR
+
+let carPosition = 56;
+
+
+
+function playerMovement(event) {
+
+    if (carPosition == 167) {
+        switch (event.key) {
+            case KEY_DOWN.toRight:
+                carPosition = carPosition + CAR_POSITION;
+                document.body.querySelector(".car-player").style.left = carPosition + "px";
+                console.log(carPosition);
+                break;
+            case KEY_DOWN.toLeft:
+                carPosition = carPosition - CAR_POSITION;
+                document.body.querySelector(".car-player").style.left = carPosition + "px";
+                console.log(carPosition);
+                break;
+        }
+    } else if (carPosition == 278) {
+        switch (event.key) {
+            case KEY_DOWN.toLeft:
+                carPosition = carPosition - CAR_POSITION;
+                document.body.querySelector(".car-player").style.left = carPosition + "px";
+                console.log(carPosition);
+                break;
+        }
+    } else if (carPosition == 56) {
+        switch (event.key) {
+            case KEY_DOWN.toRight:
+                carPosition = carPosition + CAR_POSITION;
+                document.body.querySelector(".car-player").style.left = carPosition + "px";
+                console.log(carPosition);
+                break;
+        }
+    }
+}
+
+//Invocation
+document.addEventListener('keydown', playerMovement);
+
+setInterval(highwayMovement, 120); // Invoking highwayMovement
